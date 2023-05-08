5eb8b985712e2c72f2a9036f83950d5e5e3f8abc
Tayy_Eb Chaudhary <56630383+TayyabChaudhary@users.noreply.github.com>
2023-05-08 18:20:40 +0500

    add onoff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [first, setfirst] = React.useState(localStorage.getItem("click"))
+  const [state, setstate] = React.useState(localStorage.getItem("click-second"));
+
+  const handleOnClick = () => {
+    setfirst("true");
+    setstate("false");
+    localStorage.setItem("click", first)
+    localStorage.setItem("click-second", state)
+
+
+
+  }
+
+
+  const handleSecondClick = () => {
+    setstate("true");
+    setfirst('false');
+    localStorage.setItem("click", first)
+    localStorage.setItem("click-second", state)
+    refresh()
+
+  }
+  const refresh=()=>{
+    window.location.reload();
+  }
+
+  React.useEffect(() => {
+    console.log("dfsdfdsf");
+    // window.location.reload();
+
+    // window.location.reload();
+    
+
+    window.addEventListener("storage", (first) => {
+
+    window.location.reload();
+      
+    });
+    
+   
+  }, [first,state]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {localStorage.getItem("click-second")?
+      <>
+      <button onClick={handleSecondClick}  className={first=="true" ? "active" : "off__btn"}>
+        OFF
+      </button>
+      <button className={state=="true" ? "active" : "on__btn"} onClick={handleOnClick}>
+        ON
+      </button> </>:"ghfhgf"} 
     </div>
   );
 }
