b0269e858ac722d19d90d3b97a85fa2d363b2d42
numanijaz786 <nomiijaz6224@gmail.com>
2023-05-09 19:53:19 +0500

    sockets intigreated issue resolved

--- src/App.js
+++ src/App.js
@@ -1,33 +1,95 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import socketIOClient from 'socket.io-client';
+
+const socket = socketIOClient('http://localhost:3003');
+
 
 function App() {
-  const [first, setfirst] = useState()
+  const [first, setfirst] = useState("true")
+  const [data, setData] = useState()
+  const handleOnClick = async (event) => {
+    let formData = {}
+    if (event == "button1") {
+      formData = {
+        button1: "true",
+        button2: "false",
+        button3: "false"
+      }
 
-  const handleOnClick = (state) => {
- (state=="off") ? setfirst("false") : setfirst("true");
+    }
+    else if (event == "button2") {
+      formData = {
+        button1: "false",
+        button2: "true",
+        button3: "false"
+      }
+
+    }
+    else {
+      formData = {
+        button1: "false",
+        button2: "false",
+        button3: "true"
+      }
+
+    }
+
+    await fetch('http://localhost:3003/update', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    }).then((response) => {
+      if (response.ok) {
+        console.log(response, first);
+        (first === "true") ? setfirst("false") : setfirst("true");
+        // window.addEventListener("storage", () => {
+        //   window.location.reload();
+        //   });
+
+      } else {
+        console.log('Error updating user');
+      }
+    }).catch((error) => console.error('Error:', error));
  
-    localStorage.setItem("click", state)
   }
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://localhost:3003/');
+      const jsonData = await response.json();
+      setData(jsonData?.rows[0]);
+      console.log(data, jsonData.rows);
+    }
+    fetchData();
+  }, []);
 
-  React.useEffect(() => {
-    window.addEventListener("storage", () => {
-    window.location.reload();
+ 
+
+  useEffect(() => {
+
+    socket.on('dbUpdate', (data) => {
+      setData(data)
+      console.log(data)
+
     });
-  }, [first]);
 
-  useEffect(()=>{
-    setfirst(localStorage.getItem("click")==="on"?"true":"false")
-  },[])
+    return () => {
+      socket.off('dbUpdate')
+    };
+  }, [socket]);
+
 
   return (
     <div className="App">
-      <button onClick={()=>handleOnClick("off")}  className={first=="true" ? "active" : "off__btn"}>
+      <button onClick={() => handleOnClick("button1")} className={data?.button1 !== "true" ? "active" : "off__btn"}>
         OFF
       </button>
-      <button className={first!="true" ? "active" : "on__btn"} onClick={()=>handleOnClick("on")}>
+      <button className={data?.button2 !== "true" ? "active" : "on__btn"} onClick={() => handleOnClick("button2")}>
         ON
-      </button> 
+      </button>
+      <button className={data?.button3 !== "true" ? "active" : "onof__btn"} onClick={() => handleOnClick("button3")}>
+        ONoff
+      </button>
     </div>
   );
 }
